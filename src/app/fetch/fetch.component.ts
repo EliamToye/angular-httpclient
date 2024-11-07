5cde396a036d3dd9dbde0a62e048d49f7cdf0d22
Hans Naert <hans.naert@vives.be>
2024-11-07 17:28:43 +0100

    HttpClient with HttpClientModule

---
+++ src/app/fetch/fetch.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fetch',
+  standalone: true,
+  imports: [HttpClientModule],
+  templateUrl: './fetch.component.html',
+  styleUrl: './fetch.component.scss'
+})
+export class FetchComponent {
+
+  fetch() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  constructor(private http: HttpClient) {
+   }
+
+}
